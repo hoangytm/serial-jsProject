883b32f479f70c7815a55bf10f4053ac7a50fd93
Hoang Phan <51194076+hoangytm@users.noreply.github.com>
2020-12-06 22:56:43 +0700

    complete hidden answer and question

--- question/script.js
+++ question/script.js
@@ -1,2 +1,14 @@
-let plus=document.getElementById("plus")
-let minus=document.getElementById('minus')
+let plus = document.getElementById("plus")
+let minus = document.getElementById('minus')
+
+function changeStatus(currentNode) {
+    let question = currentNode.parentElement.parentElement
+    console.log('change status',question);
+    question.classList.toggle("minus");
+    setButton(currentNode)
+}
+
+function setButton(currentNode) {
+    console.log(currentNode)
+    currentNode.classList.toggle('show-text')
+}
